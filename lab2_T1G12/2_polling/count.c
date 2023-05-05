8e4ca4a1e7aad3a16c7a5627d36cb39e4b8da9d8
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2023-05-05 16:23:58 +0200

    lab2

---
+++ lab2_T1G12/2_polling/count.c
@@ -0,0 +1,85 @@
+/* POLLING */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+/* Definitions */
+#define MAX_QUEST 5
+#define FILENAME "./votations.dat"
+
+typedef struct {
+     int idTable;
+     int idQuestion;
+     int yes;
+     int no;
+  }  tRecord;
+
+/* Main program */
+int main (int argc, char **argv)
+{
+   int    rank, nprocs;
+   int    recordsize, filenumrecords, rem, numrecords;
+   int    i, quest;
+   tRecord *buf;
+   long  total, yes[MAX_QUEST], totYes[MAX_QUEST];
+   long  no[MAX_QUEST], totNo[MAX_QUEST];
+
+   MPI_Status   info;
+   MPI_Datatype rectype; 
+   MPI_File     infile;
+   MPI_Offset   filesize, poffset;
+
+   /* MPI Initialization */
+   MPI_Init (&argc, &argv);
+   MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+   MPI_Comm_size (MPI_COMM_WORLD, &nprocs);
+
+   /* Create datatype */
+
+
+   /* Each process reads a part of the file */
+
+
+   /* Each process determines number of records to read and initial offset */
+
+
+   /* Allocate buffer for records */
+
+
+   /* Process reads numrecords consecutive elements */
+
+   /* Count results by each process */
+
+   /* Print local results */
+   printf ("Proc %3d. Counted votes = %d\n", rank, total);
+
+
+   /* Print global results on process 0 */
+
+
+
+   if (rank == 0)
+   {
+      total = 0;
+      printf ("------------------------------------------------------------\n");
+      for (i=0; i<MAX_QUEST; i++) 
+      {
+         total += totYes[i] + totNo[i];
+         printf("Question %d: yes: %.1f%% (%d) no: %.1f%% (%d)\n", i, totYes[i]*100.0/total, totYes[i], totNo[i]*100.0/total, totNo[i]);
+         fflush (stdout);
+      }
+
+      printf ("------------------------------------------------------------\n");
+      printf ("Total votes = %d\n", total);
+      fflush (stdout);
+   }
+
+   /* Free datatype */
+   MPI_Type_free (&rectype);
+
+   /* End MPI */
+   MPI_Finalize();
+   return 0;
+}
+
---
+++ lab2_T1G12/3_bugs/job.cmd
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=ex1
+#SBATCH -p ippd-cpu
+#SBATCH --output=out_bugs_%j.out
+#SBATCH --error=out_bugs_%j.err
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --time=00:00:30
+
+module load GCC/10.2.0
+module load OpenMPI
+
+make bug1 >> make.out || exit 1      # Exit if make fails
+
+mpirun -np 2 bug1
