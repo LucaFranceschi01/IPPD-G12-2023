db0a6bd6353c7b60e6cc87d5dca914d1b7345a32
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2023-05-14 13:31:28 +0200

    sendreceive creo que hecho

--- lab2_T1G12/1_rates/sendrecvp.c
+++ lab2_T1G12/1_rates/sendrecvp.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <mpi.h>
 
+#define MIN(a,b) (((a)<(b))?(a):(b)) // define min function (used for outliers)
+#define OUTLIER_ITER 10 // we get good results with 10 iterations
+
 int main (int argc, char **argv) {
 
     int rank, nprocs;
@@ -16,58 +19,61 @@ int main (int argc, char **argv) {
     int n = 1, received;
     MPI_Status status;
 
-	double wtime;
+	double wtime, local_wtime;
 	MPI_Request request = MPI_REQUEST_NULL;
     while(n < 10000000) {
-        double buff[n];
+        double buf[n];
 		// No need for initialization
-		
+
         if(rank == 0) {
 			if(n < 1000) {
-				double local_wtime = 0;
+				wtime = 0;
 				for(int i=0; i<1000/n; i++) {
 					local_wtime = MPI_Wtime();
-					MPI_Isend(&buff, n, MPI_DOUBLE, 1, 1111, MPI_COMM_WORLD, &request);
+					MPI_Isend(&buf, n, MPI_DOUBLE, 1, 1234, MPI_COMM_WORLD, &request);
 					MPI_Wait(&request, &status);
-					MPI_Irecv(&buff, n, MPI_DOUBLE, 1, 1111, MPI_COMM_WORLD, &request);
+					MPI_Irecv(&buf, n, MPI_DOUBLE, 1, 1234, MPI_COMM_WORLD, &request);
 					MPI_Wait(&request, &status);
-					wtime += MPI_Wtime() - local_wtime;
+					wtime += MPI_Wtime() - local_wtime; // add up partial values of wait time
 				}
-				wtime = wtime * n / 1000;
+				wtime = wtime * n / 1000.f; // average waiting time
 			} else {
-				wtime = MPI_Wtime();
-				MPI_Isend(&buff, n, MPI_DOUBLE, 1, 1111, MPI_COMM_WORLD, &request);
-				MPI_Wait(&request, &status);
-				MPI_Irecv(&buff, n, MPI_DOUBLE, 1, 1111, MPI_COMM_WORLD, &request);
-				MPI_Wait(&request, &status);
-				wtime = MPI_Wtime() - wtime;
+				wtime = __DBL_MAX__; // init to max double value
+				for(int i=0; i<OUTLIER_ITER; i++) { // iterate a couple of times to get rid of outliers
+					local_wtime = MPI_Wtime();
+					MPI_Isend(&buf, n, MPI_DOUBLE, 1, 1234, MPI_COMM_WORLD, &request);
+					MPI_Wait(&request, &status);
+					MPI_Irecv(&buf, n, MPI_DOUBLE, 1, 1234, MPI_COMM_WORLD, &request);
+					MPI_Wait(&request, &status);
+					wtime = MIN(wtime, MPI_Wtime() - local_wtime);
+				}
 			}
-
-			printf("%d\t%f\t%f\n", n, wtime, sizeof(buff)/(1000000*wtime));
-        }
-
-        if(rank == 1) {
+			printf("%d\t%f\t%f\n", n, wtime, sizeof(buf)/(1048576 * wtime));
+        } 
+		if(rank == 1) {
             if(n < 1000) {
-				double local_wtime = 0;
+				wtime = 0;
 				for(int i=0; i<1000/n; i++) {
 					local_wtime = MPI_Wtime();
-					MPI_Isend(&buff, n, MPI_DOUBLE, 0, 1111, MPI_COMM_WORLD, &request);
+					MPI_Irecv(&buf, n, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD, &request);
 					MPI_Wait(&request, &status);
-					MPI_Irecv(&buff, n, MPI_DOUBLE, 0, 1111, MPI_COMM_WORLD, &request);
+					MPI_Isend(&buf, n, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD, &request);
 					MPI_Wait(&request, &status);
 					wtime += MPI_Wtime() - local_wtime;
 				}
-				wtime = wtime * n / 1000;
+				wtime = wtime * n / 1000.f;
 			} else {
-				wtime = MPI_Wtime();
-				MPI_Isend(&buff, n, MPI_DOUBLE, 0, 1111, MPI_COMM_WORLD, &request);
-				MPI_Wait(&request, &status);
-				MPI_Irecv(&buff, n, MPI_DOUBLE, 0, 1111, MPI_COMM_WORLD, &request);
-				MPI_Wait(&request, &status);
-				wtime = MPI_Wtime() - wtime;
+				wtime = __DBL_MAX__;
+				for(int i=0; i<OUTLIER_ITER; i++) {
+					local_wtime = MPI_Wtime();
+					MPI_Irecv(&buf, n, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD, &request);
+					MPI_Wait(&request, &status);
+					MPI_Isend(&buf, n, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD, &request);
+					MPI_Wait(&request, &status);
+					wtime = MIN(wtime, MPI_Wtime() - local_wtime);
+				}
 			}
         }
-
 		n = n*2;
     }
 
